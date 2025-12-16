17421400c588ded04eeb1564e8496a05c4b66842
Fanny <galaxysgra@gmail.com>
2025-12-16 11:55:58 +0800

    Initial Commit

---
+++ final/finalApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalApp.swift
+//  final
+//
+//  Created by Fanny on 2025/12/16.
+//
+
+import SwiftUI
+
+@main
+struct finalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
